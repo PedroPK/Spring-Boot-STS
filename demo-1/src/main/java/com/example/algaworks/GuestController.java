43bdda5a60d79f9e0ed55314acbbc5ac7b24c908
pedropk <pedropk@gmail.com>
2018-09-07 18:52:28 -0300

    Third Commit.
    Inserting new Guest with Success!

--- demo-1/src/main/java/com/example/algaworks/GuestController.java
+++ demo-1/src/main/java/com/example/algaworks/GuestController.java
@@ -3,9 +3,11 @@ package com.example.algaworks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.algaworks.model.entity.Guest;
 import com.example.algaworks.model.repository.GuestCrudRepository;
 
 @Controller
@@ -20,7 +22,14 @@ public class GuestController {
 	public ModelAndView listGuests() {
 		ModelAndView mv = new ModelAndView("getMappingGuestList");
 		mv.addObject("guests", guestCrudRepository.findAll());
+		mv.addObject(new Guest());
 		return mv;
+	}
+	
+	@PostMapping("/insertGuest")
+	public String insertGuest(Guest pGuest) {
+		this.guestCrudRepository.save(pGuest);
+		return "redirect:/getMappingConvidados";
 	}
 
 	@RequestMapping(GUEST_LIST)
