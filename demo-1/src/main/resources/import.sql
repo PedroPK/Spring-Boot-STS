e1aa04f63aebca8371ccab8fdb08d7893e7a41e4
pedropk <pedropk@gmail.com>
2018-09-07 16:22:50 -0300

    The second commit.
    Now we have a Import.sql to do the initial population of our database.
    We now can access the Database, convert them in Object with internal
    ORM, and finally show the Data at browser, using a HTML page with
    Thymeleaf tags.
    
    Excellent!

---
+++ demo-1/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Guest (id, name, qt_companions) values (1, 'Peter', 6);
+insert into Guest (id, name, qt_companions) values (2, 'Benny', 10);
+insert into Guest (id, name, qt_companions) values (3, 'Kevin', 1);
---
+++ demo-1/src/test/java/com/example/algaworks/model/repository/GuestCrudRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.algaworks.model.repository;
+
+import org.junit.Test;
+
+public class GuestCrudRepositoryTest {
+	
+	@Test
+	public void persistGuests() {
+		
+	}
+	
+}
